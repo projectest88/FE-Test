86dcf4ed338b6a7bd311d0e841314c87c01dede0
projectest88 <andrianlimnardy88@gmail.com>
2025-02-23 22:08:01 +0700

    first commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-gray-800 text-white mt-8">
       <div class="container flex justify-center mx-auto py-3 lg:max-w-[1200px]">
-            <p>Copyright © 2024 NewsApp </p>
+            <p>Copyright © 2025 NewsApp </p>
       </div>
     </footer>
 </template>
